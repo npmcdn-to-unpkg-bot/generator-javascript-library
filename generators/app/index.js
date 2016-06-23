22efeee6293b79fa0cbbb62a9ee48bcbba0638bd
Benoit Averty <benoit.averty@zenika.com>
2016-06-23 22:08:31 +0200

    feat(generator): Add basic functionality (templates and props)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,78 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const yeoman = require('yeoman-generator');
+const chalk = require('chalk');
+const path = require('path');
+const mkdirp = require('mkdirp');
+const camelcase = require('camelcase');
 
 module.exports = yeoman.Base.extend({
+  initializing: function () {
+    this.props = {};
+  },
   prompting: function () {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the swell ' + chalk.red('generator-javascript-library') + ' generator!'
-    ));
+    this.log('Welcome to the ' + chalk.blue('javascript-library') + ' generator!');
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'libraryName',
+      message: 'What is the name of your library (your github repo should have the same name)?',
+      default: path.basename(process.cwd())
+    }, {
+      type: 'input',
+      name: 'libraryDesc',
+      message: 'Write a short description for your library.',
+      default: ''
+    }, {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'What is your github username (or organisation)?',
+      default: ''
+    }, {
+      type: 'input',
+      name: 'libraryAuthor',
+      message: 'Who\'s the author of the library?',
+      default: answers => answers.githubUsername
+    }, {
+      type: 'input',
+      name: 'license',
+      message: 'Which license do you want for your library?',
+      default: 'MIT'
     }];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
+    return this.prompt(prompts).then(answers => {
+      Object.assign(this.props, answers, {
+        githubSlug: `${answers.githubUsername}/${answers.libraryName}`,
+        camelCaseLibraryName: camelcase(answers.libraryName)
+      });
+    });
   },
-
+  default: function () {
+    if (path.basename(this.destinationPath()) !== this.props.libraryName) {
+      this.log(
+        'Your generator must be inside a folder named ' + this.props.libraryName + '\n' +
+        'I\'ll automatically create this folder.'
+      );
+      mkdirp(this.props.name);
+      this.destinationRoot(this.destinationPath(this.props.libraryName));
+    }
+  },
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('**/*'),
+      this.destinationPath(''),
+      this.props
+    );
+    this.fs.copyTpl(
+      this.templatePath('**/.*'),
+      this.destinationPath(''),
+      this.props
     );
   },
-
   install: function () {
-    this.installDependencies();
+    this.log(this.props.libraryName);
+    this.npmInstall();
+  },
+  end: function () {
+
   }
 });
---
+++ generators/app/templates/README.md
@@ -0,0 +1,7 @@
+# <%- libraryName %>
+
+<%- libraryDesc %>
+
+[![Travis](https://img.shields.io/travis/<%- githubSlug %>.svg?maxAge=2592000)](https://travis-ci.org/<%- githubSlug %>)
+[![Codecov](https://img.shields.io/codecov/c/github/<%- githubSlug %>.svg?maxAge=2592000)]()
+[![npm](https://img.shields.io/npm/v/<%- libraryName %>.svg?maxAge=2592000)](https://npmjs.com/package/<%- libraryName %>)
