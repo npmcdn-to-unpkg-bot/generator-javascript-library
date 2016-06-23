3c9865f4dc2faab003db6115ebdb5101cb5304d8
Benoit Averty <benoit.averty@zenika.com>
2016-06-23 22:40:08 +0200

    chore(build): remove linting as part of test script

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
@@ -10,10 +9,7 @@ var plumber = require('gulp-plumber');
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(excludeGitignore());
 });
 
 gulp.task('nsp', function (cb) {
